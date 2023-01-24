2682cf2f432652cad2ba366b19a1308dd1a9907e
garoh2291 <garoh2291@gmail.com>
2023-01-24 16:51:30 +0400

    finished part 2

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,7 +1,5 @@
 const { MongoClient } = require("mongodb");
 const { MONGODB_URI, DB } = require("../config/index");
-const uri =
-  "mongodb+srv://Garnik:Aa123456@cluster0.qhryhu1.mongodb.net/test/db";
 
 async function getCollection() {
   const client = await MongoClient.connect(MONGODB_URI, {
