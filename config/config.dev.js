c982bbb6dea6f1abcd5d1fd74279f43e9fc3fd73
garoh2291 <garoh2291@gmail.com>
2023-01-24 16:37:42 +0400

    finished part 2

--- config/config.dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  MONGODB_URI:
-    "mongodb+srv://Garnik:Aa123456@cluster0.qhryhu1.mongodb.net/test/db",
-  PORT: 3051,
-  DB: "db",
-  FETCH_URL: "https://catfact.ninja/breeds",
-};
